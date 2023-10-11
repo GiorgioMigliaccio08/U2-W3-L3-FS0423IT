81249eeeb85cc6af7b2fbb7e2b21c4c161339d7f
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-10-11 15:47:14 +0200

    first commit

---
+++ Libreria.js
@@ -0,0 +1,24 @@
+fetch("https://striveschool-api.herokuapp.com/books")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      if (res.status === 404) {
+        throw new Error("404 - Not Found");
+      } else if (res.status === 500) {
+        throw new Error("500 - Internal Server Error");
+      } else {
+        throw new Error("Errore generico");
+      }
+    }
+  })
+  .then((bookStores) => {
+    console.log("bookStores", bookStores);
+    const imgUno = document.getElementById("carduno");
+    const newImguno = document.createElement("img");
+    newImguno.setAttribute("src", bookdata[0].img);
+    card - body.appendChild(newImguno);
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
